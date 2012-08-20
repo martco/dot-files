2e67f36a3ab2766db9f9fc5b13b5800f26bda7a9
tUrG0n <yordanov.piotr@gmail.com>
2012-08-20 03:56:28 +0300

    Added prezo configs

---
+++ oh-my-zsh/custom/zsh/before/before.sample.zsh
@@ -0,0 +1,12 @@
+#
+# Samples of what you can do in *.before.zsh files.
+# You can create as many files as you like, or put everything in one.
+#
+
+# append your own plugins. the $plugins at the end includes the plugins
+# defined by YADR.
+
+# set your theme.
+export ZSH_THEME="tUrG0n"
+
+cd ~/.yadr
